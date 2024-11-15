8577086208c8c54c743eb62094c071f47a923ef9
simon <simon@utk.com.tw>
2024-11-15 15:01:16 +0800

    feat:更新路徑

--- app.py
+++ app.py
@@ -11,20 +11,16 @@ def index():
 def TransRecord():
     return render_template('TransRecord.html')
 
-@app.route('/queryTransRecord', methods=['POST'])
-def queryTransRecord():
+@app.route('/TransRecord/query', methods=['POST'])
+def query():
     data = request.json
     userId = data.get('userId', '').strip() or None
     startDate = data.get('startDate', '').strip() or None
     endDate = data.get('endDate', '').strip() or None
 
-    # 獲取交易記錄
     TransRecord = TransRecordService.getData(userId, startDate, endDate)
-
-    # 獲取總金額
     totalAmount = TransRecordService.getTotalAmount(userId) if TransRecord is not None else 0
 
-    # 將 DataFrame 轉換為字典列表
     records = TransRecord.to_dict(orient='records') if TransRecord is not None else []
 
     return jsonify(records=records, totalAmount=totalAmount)
